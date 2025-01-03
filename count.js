6feaa323c823bf8834c6dbd31185282f49c8617d
NguyenHuong04 <Huong.NT231594@sis.hust.edu.vn>
2025-01-03 22:52:57 +0700

    count.js

--- count.js
+++ count.js
@@ -5,24 +5,24 @@ function isBicycle(plate) {
 
 // Đếm số lượng xe đạp
 function countBicycles(vehicles) {
-    let count = 0;
+    let count1 = 0;
     for (let i = 0; i < vehicles.length; i++) {
         if (isBicycle(vehicles[i].plate)) {
-            count++;
+            count1++;
         }
     }
-    return count;
+    return count1;
 }
 
 // Đếm số lượng xe máy
 function countMotorcycles(vehicles) {
-    let count = 0;
+    let count2 = 0;
     for (let i = 0; i < vehicles.length; i++) {
         if (!isBicycle(vehicles[i].plate)) {
-            count++;
+            count2++;
         }
     }
-    return count;
+    return count2;
 }
 
 // Xuất các hàm để sử dụng ở file khác
